ecce2e77a8a66e8efa98fd7e4e9aa15ece54e2e8
郑涛 <wavesbig@qq.com>
2025-11-05 13:54:13 +0800

    fix: 去除面包屑

--- src/renderer/src/layouts/components/SiteHeader/index.tsx
+++ src/renderer/src/layouts/components/SiteHeader/index.tsx
@@ -1,13 +1,3 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@renderer/components/ui/breadcrumb'
-
-import { Separator } from '@renderer/components/ui/separator'
 import { SidebarTrigger } from '@renderer/components/ui/sidebar'
 import { ReactNode } from 'react'
 
@@ -16,7 +6,7 @@ export default function SiteHeader(): ReactNode {
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+        {/* <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem className="hidden md:block">
@@ -27,7 +17,7 @@ export default function SiteHeader(): ReactNode {
               <BreadcrumbPage>Data Fetching</BreadcrumbPage>
             </BreadcrumbItem>
           </BreadcrumbList>
-        </Breadcrumb>
+        </Breadcrumb> */}
       </div>
     </header>
   )
