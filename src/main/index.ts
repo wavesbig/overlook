392d5e57cfbd4df9a3897b1349d3eb899c4f7430
郑涛 <wavesbig@qq.com>
2025-11-09 10:11:26 +0800

    feat: 文章说明

--- src/main/index.ts
+++ src/main/index.ts
@@ -50,7 +50,8 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  // Align AppUserModelId with electron-builder appId to avoid Windows shortcut/icon issues
+  electronApp.setAppUserModelId('com.overlook.app')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
